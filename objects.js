71e2610f44a1be3f6067e3f0a9f52a4050f9c87e
Kyle Nielsen <nielsen.kyle93@utexas.edu>
2017-10-31 17:50:50 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -6,5 +6,5 @@ function updatePlaylist (playlist, artistName, songTitle) {
 
 function removeFromPlaylist (playlist, artistName) {
   delete playlist.artistName
-  return Object.assign({}, playlist)
+  return playlist.artistName
 }
